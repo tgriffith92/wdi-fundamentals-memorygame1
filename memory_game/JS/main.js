6733685b4d8fb7ffdbf2c5908d6545ace1792449
Terence Griffith <terencegriffithCB5@MacBook-Air-9.local>
2019-07-08 22:12:00 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,18 +1,22 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cards[0]);
-cardsInPlay.push(cards[2]);
-
-if(cardsInPlay.length === 2) {
- if(cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
- } else {
-     alert("Sorry, try again.");
- }  
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
 }
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
+function flipCard(cardId) {
+    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId]);
+    if(cardsInPlay.length === 2) {
+        checkForMatch(); 
+       }
+       
+}
+flipCard(0);
+flipCard(2);
+
