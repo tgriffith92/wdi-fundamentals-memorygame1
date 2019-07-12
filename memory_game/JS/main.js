3c33fdeaf1d0417fc509aef15c0816e0f8398a71
Terence Griffith <terencegriffithCB5@MacBook-Air-9.local>
2019-07-11 21:45:09 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,17 +1,40 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "/Images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "/Images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "/Images/queen-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "/Images/queen-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+        alert("You found a match!");
       } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
       }
 }
 
 function flipCard(cardId) {
-    cardsInPlay.push(cards[cardId]);
-    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     if(cardsInPlay.length === 2) {
         checkForMatch(); 
        }
